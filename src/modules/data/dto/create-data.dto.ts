ef1aaf3a59c2878a56cbc2e2285589e684e45776
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-09-10 12:03:02 +0800

    Configured Webhook

--- src/modules/data/dto/create-data.dto.ts
+++ src/modules/data/dto/create-data.dto.ts
@@ -6,24 +6,31 @@ export class DataDTO {
     @IsNotEmpty({ message: "rvmID should not be empty" })
     rvmID: string
 
-    @IsString()
+    @IsString({ message: "timestamp must be a string" })
+    @IsNotEmpty({ message: "timestamp should not be empty" })
     timestamp: string
 
-    @IsString()
+    @IsString({ message: "totalCount must be a string" })
+    @IsNotEmpty({ message: "totalCount should not be empty" })
     totalCount: string
 
-    @IsString()
+    @IsString({ message: "totalValue must be a string" })
+    @IsNotEmpty({ message: "totalValue should not be empty" })
     totalValue: string
 
-    @IsString()
+    @IsString({ message: "item must be a string" })
+    @IsNotEmpty({ message: "item should not be empty" })
     item: string
 
-    @IsString()
+    @IsString({ message: "messageID must be a string" })
+    @IsNotEmpty({ message: "messageID should not be empty" })
     messageID: string
 
-    @IsString()
+    @IsString({ message: "userID must be a string" })
+    @IsNotEmpty({ message: "userID should not be empty" })
     userID: string
 
-    @IsString()
+    @IsString({ message: "sign must be a string" })
+    @IsNotEmpty({ message: "sign should not be empty" })
     sign: string
 }
