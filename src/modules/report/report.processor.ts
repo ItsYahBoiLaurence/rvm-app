1401567078777ec8f159923f5a169b41ee1ecf47
ItsYahBoiLaurence <laurence@mayan.com.ph>
2026-01-19 10:23:27 +0800

    Update report processor

--- src/modules/report/report.processor.ts
+++ src/modules/report/report.processor.ts
@@ -17,18 +17,18 @@ export class GenerateReportQueue extends WorkerHost {
 
   async process(job: Job) {
     this.logger.log(job.name);
-    const rawExcelData = await this.reportService.generateExcelData();
-    const excelFile = await this.excelService.generateExcelFile(rawExcelData);
+    // const rawExcelData = await this.reportService.generateExcelData();
+    // const excelFile = await this.excelService.generateExcelFile(rawExcelData);
 
-    try {
-      const response = await this.reportService.sendExcelFileToOtherApi(
-        Buffer.from(excelFile),
-      );
-      this.logger.log('***************************');
-      this.logger.log(response);
-      this.logger.log('***************************');
-    } catch (e) {
-      this.logger.error(e);
-    }
+    // try {
+    //   const response = await this.reportService.sendExcelFileToOtherApi(
+    //     Buffer.from(excelFile),
+    //   );
+    //   this.logger.log('***************************');
+    //   this.logger.log(response);
+    //   this.logger.log('***************************');
+    // } catch (e) {
+    //   this.logger.error(e);
+    // }
   }
 }
